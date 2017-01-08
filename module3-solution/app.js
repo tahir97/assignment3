9b496982c8b109bfed56ee52c76ceb101e903a6a
tahir97 <mtahiridrees@gmail.com>
2017-01-08 22:30:50 +0500

    code updated

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,19 +4,37 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.directive('foundItems', foundItemsDirective)
+// .directive('foundItems', foundItemsDirective)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var narrowCtrl = this;
   narrowCtrl.menuItem = "";
+  narrowCtrl.found = [];
+  narrowCtrl.nothingFoundFlag = false;
 
   narrowCtrl.getMatchedItems = function () {
-    narrowCtrl.found = MenuSearchService.getMatchedMenuItems(narrowCtrl.menuItem);
+    if (narrowCtrl.menuItem == "") {
+       narrowCtrl.nothingFoundFlag = true;
+    } else {
+
+    var promise = MenuSearchService.getMatchedMenuItems(narrowCtrl.menuItem);
+    promise.then(function (result) {
+      narrowCtrl.found = MenuSearchService.getFoundItems(result,narrowCtrl.menuItem);
+      if (narrowCtrl.found.length != 0) {
+        narrowCtrl.nothingFoundFlag = false;
+      } else {
+         narrowCtrl.nothingFoundFlag = true;
+      }
+     }, function (errorResponse) {
+       console.log(errorResponse.message);
+     });
+   }
   }
 
   narrowCtrl.removeItem = function (itemIndex) {
+    narrowCtrl.found.splice(itemIndex, 1);
   };
 }
 
@@ -24,53 +42,53 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http,ApiBasePath) {
   var service = this;
 
-
-  service.getMatchedMenuItems = function (searchTerm) {
-    return $http({
-      method: "GET",
-      url: ApiBasePath
-    }).then(function (result) {
-      var foundItems = [];
-      for (var i=0; i < result.data.menu_items.length; i++) {
-        //debugger;
-        if (result.data.menu_items[i].description.indexOf(searchTerm) !== -1) {
-          foundItems.push(result.data.menu_items[i]);
+service.getFoundItems = function (itemsList,searchTerm) {
+  var foundItems = [];
+      for (var i=0; i < itemsList.data.menu_items.length; i++) {
+        if (itemsList.data.menu_items[i].description.indexOf(searchTerm) !== -1) {
+          foundItems.push(itemsList.data.menu_items[i]);
         }
       }
       return foundItems;
-  })
+}
+  service.getMatchedMenuItems = function (searchTerm) {
+     var jsonItems = $http({
+      method: "GET",
+      url: ApiBasePath
+    });
+  return jsonItems;
   };
 }
 
-function foundItemsDirective() {
-  var ddo = {
-    templateUrl: 'foundItems.html',
-    scope: {
-      found: '<',
-      onRemove: '&'
-    }
-    //controller: foundItemsDirectiveController,
-    //controllerAs: 'list',
-    //bindToController: true
-  };
-
-  return ddo;
-}
-
-function foundItemsDirectiveController() {
-  var list = this;
-
-  // list.cookiesInList = function () {
-  //   for (var i = 0; i < list.items.length; i++) {
-  //     var name = list.items[i].name;
-  //     if (name.toLowerCase().indexOf("cookie") !== -1) {
-  //       return true;
-  //     }
-  //   }
-  //
-  //   return false;
-  // };
-}
-
+// function foundItemsDirective() {
+//   var ddo = {
+//     templateUrl: '<ol> <li ng-repeat="item in narrowCtrl.found"> {{ item.name }}        <button ng-click="narrowCtrl.onRemove({index: $index});">Remove Item</button></li></ol>',//foundItems.html',
+//     scope: {
+//       found: '<',
+//       onRemove: '&'
+//     }
+//     //controller: foundItemsDirectiveController,
+//     //controllerAs: 'list',
+//     //bindToController: true
+//   };
+//
+//   return ddo;
+// }
+//
+// function foundItemsDirectiveController() {
+//   var list = this;
+//
+//   // list.cookiesInList = function () {
+//   //   for (var i = 0; i < list.items.length; i++) {
+//   //     var name = list.items[i].name;
+//   //     if (name.toLowerCase().indexOf("cookie") !== -1) {
+//   //       return true;
+//   //     }
+//   //   }
+//   //
+//   //   return false;
+//   // };
+// }
+//
 
 })();
